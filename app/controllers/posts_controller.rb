9d8f5ddef973c238922e0860bb07879e8b1356b1
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-10-02 02:34:03 +0300

    ADD create action on post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+  before_action :authenticate_user!, except: %i[index show]
   def index
     if user_signed_in?
       @id = current_user.id
@@ -12,5 +12,24 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    # @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: 'Rumor is successfully created.'
+    else
+      str = 'Rumor not saved!'
+      new_line = '<br/>'
+      @post.errors.full_messages.each do |msg|
+        str << new_line
+        str << msg
+      end
+      respond_to do |format|
+        format.html { redirect_to posts_path, alert: str }
+      end
+    end
+  end
+
+  def post_params
+    params.permit(:title, :content)
   end
 end
